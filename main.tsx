585442709a576552f4f4bbc845dd9a03852dc03d
UsmonHamidulloh <usman.x.0221@gmail.com>
2022-10-13 20:32:21 +0500

    hello world!

---
+++ main.tsx
@@ -0,0 +1,16 @@
+import blog, { ga, redirects, h } from "blog";
+
+blog({
+  title: "Usmon Hamidulloh",
+  // description: "Linux lover especially Arch Linux",
+  avatar: "./assets/usmon-hamidulloh.jpg",
+  avatarClass: "rounded-full",
+  author: "Usmon Hamidulloh",
+  lang: "en",
+  links: [
+    {title: "Email", url: "mailto:contact@usmon.me"},
+    {title: "Github", url: "https://github.com/UsmonHamidulloh"},
+    {title: "Twitter", url: "https://twitter.com/UsmonHamidulloh"},
+    {title: "Linkedin", url: "https://www.linkedin.com/in/usman-hamidulloh/"}
+  ]
+});
